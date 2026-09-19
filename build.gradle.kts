dc3c00f1a3748822b89345f497fbc7ecaebb3788
alejandro <aguerreroegido@gmail.com>
2026-09-19 21:05:20 +0100

    Set up initial Gradle project with Kotlin configuration and basic structure.

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.20"
+}
+
+group = "com.alejandro"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
